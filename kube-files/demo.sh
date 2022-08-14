2df8da83596ca65e35faeb0897be78a6892768ae
rajkiranrj <rajkirannarala@gmail.com>
2022-08-14 08:01:33 +0000

    env update

--- kube-files/demo.sh
+++ kube-files/demo.sh
@@ -5,6 +5,7 @@ kubectl create ns final
 kubectl create -f secret-data.yaml -n final
 kubectl create -f mysql-pod-definition.yaml -n final
 kubectl create -f mysql-service.yaml -n final
+kubectl create -f configmap.yaml -n final
 kubectl create -f webapp-pod-definition.yaml -n final
 kubectl create -f webapp-service.yaml -n final
 kubectl get all -n final
